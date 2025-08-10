ccb6a1539f5e6643a79d731f0eecb7153e479d19
KGN0826 <rjsska8026@gmail.com>
2025-08-10 23:21:24 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 134 ms, Memory: 36,736 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -10,9 +10,8 @@ public class Solution {
 		for(int tc=1;tc<=10;tc++) {
 			int t = Integer.parseInt(br.readLine());
 			int[][] arr = new int[100][100];
-			int[] dr = {0,0,-1};
-			int[] dc = {-1,1,0};
 			int result = 0;
+			boolean flag = true;
 			
 			for(int i=0;i<100;i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,57 +24,26 @@ public class Solution {
 				if(arr[99][j] == 2) {
 					int r = 99;
 					int c = j;
-					while(true) {
+					while(flag) {
 						if(c>0 && arr[r][c-1] == 1) {
-							r = r+dr[0];
-							c = c+dc[0];
+							c--;
 							while(c>0 && arr[r][c-1] != 0) {
-								r = r+dr[0];
-								c = c+dc[0];
+								c--;
 							}
-							r = r+dr[2];
-							c = c+dc[2];
+							r--;
 						}else if(c<arr.length-1 && arr[r][c+1] == 1) {
-							r = r+dr[1];
-							c = c+dc[1];
+							c++;
 							while(c<arr.length-1 &&arr[r][c+1] != 0) {
-								r = r+dr[1];
-								c = c+dc[1];
+								c++;
 							}
-							r = r+dr[2];
-							c = c+dc[2];
-						}
-						if(c==0) {
-							while(c<arr.length-1 && arr[r][c+1] != 1) {
-								if(r == 0) {
-									result = c;
-									break;
-								}
-								r = r+dr[2];
-								c = c+dc[2];
-							}
-						}else if(c==arr.length-1) {
-							while(c>0 && arr[r][c-1] != 1) {
-								if(r == 0) {
-									result = c;
-									break;
-								}
-								r = r+dr[2];
-								c = c+dc[2];
-							}
+							r--;
 						}else {
-							while(c>0 && c<arr.length-1 && arr[r][c-1] != 1 && arr[r][c+1] != 1) {
-								if(r == 0) {
-									result = c;
-									break;
-								}
-								r = r+dr[2];
-								c = c+dc[2];
+							if(r == 0) {
+								result = c;
+								flag = false;
+								break;
 							}
-						}
-						if(r == 0) {
-							result = c;
-							break;
+							r--;
 						}
 					}
 				}
