576ee754af48c6174611226491a86a93a4749e54
KGN0826 <rjsska8026@gmail.com>
2025-08-16 20:43:25 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int[][] arr = new int [100][100];
+    	
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	for(int i=0;i<n;i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		int n1 = Integer.parseInt(st.nextToken());
+    		int n2 = Integer.parseInt(st.nextToken());
+    		
+    		for(int a=0;a<10;a++) {
+    			for(int b=0;b<10;b++) {
+    				arr[n1+a][n2+b] += 1;
+    			}
+    		}
+    	}
+    	int result = 0;
+    	for(int a=0;a<100;a++) {
+    		for(int b=0;b<100;b++) {
+    			if(arr[a][b] != 0) {
+    				result ++;
+    			}
+    		}
+    	}
+    	System.out.println(result);
+    }
+}
+
