42b30add31ab5a1f5fbd5599d43074719392496f
KGN0826 <rjsska8026@gmail.com>
2025-09-03 02:25:39 +0900

    [Gold V] Title: 탑, Time: 912 ms, Memory: 88960 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = 1; i <= N; i++) {
+            int currentHeight = Integer.parseInt(st.nextToken());
+
+            // 스택을 확인하며 현재 탑보다 낮은 탑 제거
+            while (!stack.isEmpty()) {
+                int[] top = stack.peek();
+                int topIndex = top[0];
+                int topHeight = top[1];
+
+                // 수신 탑 확인
+                if (topHeight >= currentHeight) {
+                    sb.append(topIndex).append(" ");
+                    break; 
+                } else {
+                    //맨 위 탑 확인 후 적으면 제거
+                    stack.pop();
+                }
+            }
+
+            //수신 탑이 있는지 확인
+            if (stack.isEmpty()) {
+                sb.append(0).append(" ");
+            }
+
+            // 현재 탑 추가
+            stack.push(new int[]{i, currentHeight});
+        }
+
+        System.out.println(sb);
+    }
+}
+
