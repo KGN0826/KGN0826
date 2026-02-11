83a870c88de45da373ad60f1d71b4c86c0c7f8bf
KGN0826 <rjsska8026@gmail.com>
2026-02-11 14:49:36 +0900

    [Gold IV] Title: 수열의 점수, Time: 280 ms, Memory: 24228 KB -BaekjoonHub

--- 백준/Gold/2036. 수열의 점수/수열의 점수.java
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.java
@@ -3,9 +3,8 @@
  * 한 정수 or 두 정수를 제거 가능
  * 한 정수 -> 그 정수가 점수
  * 두 정수 -> 두 정수의 곱이 점수
- * 수열에 아무 수도 남지 않게 되었을 때 점수의 총 합?
+ * 수열에 아무 수도 남지 않게 되었을 때 점수의 총 합
  * 만약 1이면 곱하는게 더 손해임
- * 음수면 무조건 음수끼리 곱하거나 하나해서 더 해야함
  */
 
 import java.io.*;
@@ -23,26 +22,31 @@ public class Main {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		long result = 0;
+		long result = 0; //int 값 벗어나니까 long으로
 		
 		Arrays.sort(arr);
 		boolean[] visited = new boolean[n];
 		
-		//음수일때만
+		//음수일때만 먼저 계산
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i] < 0) {
 				if(visited[i]) continue;
 				
+				//음수 2개일 때
 				if(i+1 < n && arr[i+1] <= 0) {
 					visited[i] = true;
 					visited[i+1]= true;
 					result += (long) arr[i]*arr[i+1];
-				}else if(i+1 < n && arr[i+1] > 0){
+				}
+				//음수 하나에 다음꺼 양수면 음수 더해버리기
+				else if(i+1 < n && arr[i+1] > 0){
 					visited[i] = true;
 					result += arr[i];
-					break;
+					break; //양수 나왔으니까 더 해볼 필요 X
 				}
-			}else {
+			}
+			//양수 나왔으면 더 해볼 필요가 없다.
+			else {
 				break;
 			}
 		}
@@ -52,21 +56,27 @@ public class Main {
 			if(arr[i]>0) {
 				if(visited[i]) continue;
 				
+				// 둘 다 양수이면서 1이 아닐때
 				if(i > 0 && arr[i-1] > 1 && !visited[i-1]) {
 					visited[i] = true;
 					visited[i-1]= true;
 					result += (long) arr[i]*arr[i-1];
-				}else if(i > 0 && arr[i-1] >= 0 && !visited[i-1]){
+				}
+				//1이 포함될 때
+				else if(i > 0 && arr[i-1] >= 0 && !visited[i-1]){
 					visited[i] = true;
 					visited[i-1] = true;
 					result += arr[i] + arr[i-1];
-				}else if(i > 0 && arr[i-1] < 0 && !visited[i-1]){
+				}
+				// 다음 수가 음수일때
+				else if(i > 0 && arr[i-1] < 0 && !visited[i-1]){
 					visited[i] = true;
 					result += arr[i];
 				}
 			}
 		}
 		
+		//혹시 방문 안 한거는 다 더해주기
 		for(int i=0;i<arr.length;i++) {
 			if(visited[i] == false) {
 				result += arr[i];
